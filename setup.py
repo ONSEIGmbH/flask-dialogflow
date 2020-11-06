f44a3ad29692385c7213cc1c9d3e0787ed9c5d87
alexander <alexander.polzin@big-picture.com>
2020-11-06 12:38:42 +0100

    ghetto

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'dataclasses==0.6.0;python_version<"3.9"',
         'Flask==1.0.2',
-        'marshmallow==3.9.0',
+        'marshmallow==3.0.0rc5',
         'marshmallow-enum==1.5.1',
         'PyYAML==5.1',
         'tabulate==0.8.3',
